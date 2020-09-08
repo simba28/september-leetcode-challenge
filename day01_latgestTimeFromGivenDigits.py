ca65beabb7d8c89c676c776b202a5471f0ad45fd
Sudhanshu Kumar <sudh2810@gmail.com>
2020-09-08 22:36:48 +0530

    codes added

--- day01_latgestTimeFromGivenDigits.py
+++ day01_latgestTimeFromGivenDigits.py
@@ -1,3 +1,12 @@
+'''
+Given an array of 4 digits, return the largest 24 hour time that can be made.
+
+The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a 
+time is larger if more time has elapsed since midnight.
+
+Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
+'''
+
 import itertools
 
 class Solution:
